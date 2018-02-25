8e7b04c1ecaf975baa74262c8f7d0827ff10e23d
Dmitry Shkalkov <shkalkov.dima@gmail.com>
2018-02-25 22:57:58 +0300

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  if (number >= 1 && number <= 1e8) {
+    var countOfZeros = 0;
+    while (number >= 5) {
+      countOfZeros = countOfZeros + Math.floor(number / 5);
+      number = Math.floor(number / 5);
+    }
+    return countOfZeros;
+  }
+  return;
 }
